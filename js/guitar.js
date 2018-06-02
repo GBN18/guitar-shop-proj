733fbe9bccc673854bd8f6508dfe5fd1a4d78b86
GBN <gavriel@epoch.co.il>
2018-06-02 15:56:51 +0300

    Added guitar db

---
+++ js/guitar.js
@@ -0,0 +1,80 @@
+const guitars = [
+    {
+        id: 1,
+        category: 'classic',
+        name: "normal guitar",
+        description: "just a normal guitar, nothing more."
+    },
+    {
+        id: 2,
+        category: 'electric',
+        name: "electric guitar",
+        description: "bla bla"
+    },
+    {
+        id: 3,
+        category: 'electric',
+        name: "black electric guitar",
+        description: "bla bla"
+    },
+    {
+        id: 4,
+        category: 'electric',
+        name: "red electric guitar",
+        description: "bla bla"
+    },
+    {
+        id: 5,
+        category: 'special',
+        name: "guitar form hell",
+        description: "bla bla"
+    },
+    {
+        id: 6,
+        category: 'electric',
+        name: "azure electric guitar",
+        description: "bla bla"
+    },
+    {
+        id: 7,
+        category: 'electric',
+        name: "green electric guitar",
+        description: "bla bla"
+    },
+    {
+        id: 8,
+        category: 'electric',
+        name: "small white electric guitar",
+        description: "bla bla"
+    },
+    {
+        id: 9,
+        category: 'electric',
+        name: "blue electric guitar",
+        description: "bla bla"
+    },
+    {
+        id: 10,
+        category: 'electric',
+        name: "white electric guitar",
+        description: "bla bla"
+    },
+    {
+        id: 11,
+        category: 'electric',
+        name: "homemade electric guitar",
+        description: "bla bla"
+    },
+    {
+        id: 12,
+        category: 'electric',
+        name: "purple",
+        description: "bla bla"
+    },
+    {
+        id: 13,
+        category: 'classic',
+        name: "double guitar",
+        description: "bla bla"
+    },
+];
